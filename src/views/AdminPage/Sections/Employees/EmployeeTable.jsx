031db2b126255e71a989f6a8891b5e3549c3065f
renzo-c <rcnavarrop@gmail.com>
2019-01-06 18:23:53 -0500

    working on pagination

--- src/views/AdminPage/Sections/Employees/EmployeeTable.jsx
+++ src/views/AdminPage/Sections/Employees/EmployeeTable.jsx
@@ -1,39 +1,34 @@
 import React from "react";
-import { Query } from "react-apollo";
-import { GET_EMPLOYEES } from "../../../../queries/employee";
 import Row from "./Row";
 
-const EmployeesTable = () => (
-  <Query query={GET_EMPLOYEES}>
-    {({ loading, error, data }) => {
-      if (loading) return "Loading";
-      if (error) return `Error ${error.message}`;
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>NOMBRE</th>
-              <th>APELLIDO</th>
-              <th>USUARIO</th>
-              <th>ESTADO</th>
-              <th>ACCIONES</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.employees.map((employee, index) => (
-              <Row
-                key={index}
-                value={employee}
-                index={index}
-                employee={employee}
-              />
-            ))}
-          </tbody>
-        </table>
-      );
-    }}
-  </Query>
-);
+
+const EmployeesTable = ({ data }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>NOMBRE</th>
+            <th>APELLIDO</th>
+            <th>USUARIO</th>
+            <th>ESTADO</th>
+            <th>ACCIONES</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.employees.map((employee, index) => (
+            <Row
+              key={index}
+              value={employee}
+              index={index}
+              employee={employee}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default EmployeesTable;
--- src/views/AdminPage/Sections/Employees/Index.jsx
+++ src/views/AdminPage/Sections/Employees/Index.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import EmployeeTable from "./EmployeeTable";
 import Add from "../../../../components/Modal/employee/Add";
+// import Components from '../../../Components/Components';
+
+import { Query } from "react-apollo";
+import { GET_EMPLOYEES } from "../../../../queries/employee";
 
 class IndexEmployee extends Component {
   render() {
     return (
       <div>
         <h1>Empleados</h1>
-        <Add />
-        <EmployeeTable />
+        <Query query={GET_EMPLOYEES}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading";
+            if (error) return `Error ${error.message}`;
+            return (
+              <div>
+                <Add data={data}/>
+                <EmployeeTable data={data}/>
+              </div>
+            );
+          }}
+        </Query>
+        {/* <Components/> */}
       </div>
     );
   }
