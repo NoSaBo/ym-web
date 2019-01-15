b4e633354d0b0631f659aef1eac864a26a7a2255
Jesus <jesus.reyponce@gmail.com>
2019-01-15 16:00:01 -0500

    adding build directory for tests

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
-import Components from "views/Components/Components.jsx";
+// import Components from "views/Components/Components.jsx";
 import LandingPage from "views/LandingPage/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
-import LoginPage from "views/LoginPage/LoginPage.jsx";
+// import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+// import LoginPage from "views/LoginPage/LoginPage.jsx";
 import AdminPage from "views/AdminPage/AdminPage.jsx";
 import Employees from "views/AdminPage/Sections/Employees/Index";
 import Branches from "views/AdminPage/Sections/Branches/Branches.jsx";
@@ -9,13 +9,17 @@ import ServiceShifts from "views/AdminPage/Sections/ServiceShifts/ServiceShifts.
 
 var indexRoutes = [
   { path: "/", name: "LandingPage", component: LandingPage },
-  { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
-  { path: "/login-page", name: "LoginPage", component: LoginPage },
-  { path: "/components", name: "Components", component: Components },
-  { path: "/admin-page", name:"AdminPage", component: AdminPage },
-  { path: "/admin-page/employees", name:"Employees", component: Employees },
-  { path: "/admin-page/branches", name:"Branches", component: Branches },
-  { path: "/admin-page/serviceshifts", name:"ServiceShifts", component: ServiceShifts },
+  // { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
+  // { path: "/login-page", name: "LoginPage", component: LoginPage },
+  // { path: "/components", name: "Components", component: Components },
+  { path: "/admin-page", name: "AdminPage", component: AdminPage },
+  { path: "/admin-page/employees", name: "Employees", component: Employees },
+  { path: "/admin-page/branches", name: "Branches", component: Branches },
+  {
+    path: "/admin-page/serviceshifts",
+    name: "ServiceShifts",
+    component: ServiceShifts
+  }
 ];
 
 export default indexRoutes;
