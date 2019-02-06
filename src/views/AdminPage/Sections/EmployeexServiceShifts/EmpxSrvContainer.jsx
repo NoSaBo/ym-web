18a5581fd00c6325665ea1a2210b1ef811e66972
bigeyestoad <rcnavarrop@gmail.com>
2019-02-06 17:51:44 -0500

    mutatation for deleting employeexserviceshift working fine

--- src/views/AdminPage/Sections/EmployeexServiceShifts/EmpxSrvContainer.jsx
+++ src/views/AdminPage/Sections/EmployeexServiceShifts/EmpxSrvContainer.jsx
@@ -96,7 +96,6 @@ class EmpxSrvContainer extends Component {
   }
 
   buildNewAllEmpxSrv(allEmpxSrv) {
-    console.log("allEmpxSrv", allEmpxSrv);
     allEmpxSrv.map(empxsrv => {
       let branch = this.getBranchName(empxsrv.serviceshiftId);
       let begindate = this.getBegindate(empxsrv.serviceshiftId);
